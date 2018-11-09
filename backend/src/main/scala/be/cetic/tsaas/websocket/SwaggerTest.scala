fa7b27f3901a7bccaa7166b40ccdb1999e23e126
Terence Delsate <terence.delsate@cetic.be>
2018-11-09 11:49:09 +0100

    Swagger doc in progress + refacto stream action routes

--- backend/src/main/scala/be/cetic/tsaas/websocket/SwaggerTest.scala
+++ backend/src/main/scala/be/cetic/tsaas/websocket/SwaggerTest.scala
@@ -1,48 +1,15 @@
 package be.cetic.tsaas.websocket
 
-import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
 import com.github.swagger.akka.SwaggerHttpService
-import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.media.{Content, Schema}
-import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.models.ExternalDocumentation
 
-
-import javax.ws.rs.{GET, Path}
-
-@Path("/root")
-class SwaggerTest {
-
-  @GET
-  def getString(s: String) = get(complete(s))
-
-  @Path("/sub")
-  @Operation()
-  @GET
-  def subPath() = path("sub")(getString("sub"))
-
-
-
-  def swRoutesGen() = pathPrefix("root") {
-    pathEnd {
-      getString("root") ~ subPath()
-    }
-  }
-
-
-  val swRoutes: Route = swRoutesGen()
-
-
-}
-
 object SwaggerDocService extends SwaggerHttpService {
-  override val apiClasses = Set(classOf[WebSocketService])
+  override val apiClasses = Set(classOf[WebSocketService], classOf[StreamOperation])
   override val host = "localhost:8080"
 
 
   override val externalDocs = Some(new ExternalDocumentation().description("Core Docs").url("http://acme.com/docs"))
 
   //override val securitySchemeDefinitions = Map("basicAuth" -> new BasicAuthDefinition())
-  override val unwantedDefinitions = Seq("Function1", "Function1RequestContextFutureRouteResult")
+  override val unwantedDefinitions = Seq("Function1", "Function1RequestContextFutureRouteResult", "Function1RequestContextFutureRouteResult")
 }
--- backend/src/main/scala/be/cetic/tsaas/websocket/WebsocketActorJsonProtocol.scala
+++ backend/src/main/scala/be/cetic/tsaas/websocket/WebsocketActorJsonProtocol.scala
@@ -1,8 +1,31 @@
 package be.cetic.tsaas.websocket
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import spray.json.{DefaultJsonProtocol, RootJsonFormat}
+import be.cetic.tsaas.websocket.WebsocketActor.{Start, StatusRequest, Stop, Validate}
+import spray.json.{DefaultJsonProtocol, DeserializationException, JsObject, JsString, JsValue, RootJsonFormat}
 
 trait WebsocketActorJsonProtocol extends DefaultJsonProtocol with SprayJsonSupport{
   implicit val statusJsonFormat : RootJsonFormat[WebsocketActor.Status] = jsonFormat3(WebsocketActor.Status)
+
+  implicit object OperationJsonFormat extends RootJsonFormat[WebsocketActor.Operation]{
+    def write(obj: WebsocketActor.Operation): JsValue ={
+      val value= obj match {
+        case Stop => "stop"
+        case Start => "start"
+        case StatusRequest => "status"
+        case Validate => "validate"
+        case m => throw DeserializationException(s"${m} deserialization not implemented")
+      }
+      JsObject(Map("action"->JsString(value)))
+    }
+
+    override def read(json: JsValue): WebsocketActor.Operation = {
+      json.asJsObject.fields("action").asInstanceOf[JsString].value match{
+        case "stop" => Stop
+        case "start" => Start
+        case "status" => StatusRequest
+        case "validate" => Validate
+      }
+    }
+  }
 }
